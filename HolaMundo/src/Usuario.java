6d06e2b57b46c40106f0ad70b269fbaf96711660
emarcas <emarcas22@gmail.com>
2024-03-05 20:05:14 +0100

    Segundo commit

---
+++ HolaMundo/src/Usuario.java
@@ -0,0 +1,45 @@
+public class Usuario {
+    String nombre;
+    String apeliido;
+    String email;
+    int edad;
+
+    public Usuario(String nombre, String apeliido, String email, int edad) {
+        this.nombre = nombre;
+        this.apeliido = apeliido;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApeliido() {
+        return apeliido;
+    }
+
+    public void setApeliido(String apeliido) {
+        this.apeliido = apeliido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
